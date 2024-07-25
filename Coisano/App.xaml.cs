4d0cb56efadb9b69224eb61b99662bf9eef1714d
Eduardo <dukahenrick06@gmail.com>
2024-07-25 10:34:37 -0300

    isso

---
+++ Coisano/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Coisano;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Coisano/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Coisano;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
