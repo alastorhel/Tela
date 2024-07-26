2d184f07d391afe110a4d1335d80dfda3fb5a2e4
Eduardo <dukahenrick06@gmail.com>
2024-07-26 08:58:35 -0300

    oaoa

---
+++ Coisano/CadastrodoProduto.xaml.cs
@@ -0,0 +1,14 @@
+using Microsoft.Maui.Controls;
+
+namespace Coisano
+{
+    public partial class CadastrodoProduto : ContentPage
+    {
+        public CadastrodoProduto()
+        {
+            InitializeComponent();
+        }
+
+        // Adicione aqui métodos de evento para os botões, se necessário.
+    }
+}
