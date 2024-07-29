c7112880bd8b83136d93a8ca1d187d43ac0218ee
Eduardo <dukahenrick06@gmail.com>
2024-07-29 09:03:31 -0300

    fiz coisas

---
+++ Coisano/MédiaDiaria.xaml.cs
@@ -0,0 +1,19 @@
+using Microsoft.Maui.Controls;
+
+namespace Coisano
+{
+    public partial class MédiaDiaria : ContentPage
+    {
+        public MédiaDiaria()
+        {
+            InitializeComponent();
+        }
+
+        // Event handlers for the buttons can be added here
+        // For example:
+        // private void OnSaveAndContinueButtonClicked(object sender, EventArgs e)
+        // {
+        //     // Handle save and continue button click
+        // }
+    }
+}
---
+++ Coisano/Viabilidade.xaml.cs
@@ -0,0 +1,24 @@
+using Microsoft.Maui.Controls;
+
+namespace Coisano
+{
+    public partial class Viabilidade : ContentPage
+    {
+        public Viabilidade()
+        {
+            InitializeComponent();
+        }
+
+        // Event handlers for the buttons can be added here
+        // For example:
+        // private void OnCancelButtonClicked(object sender, EventArgs e)
+        // {
+        //     // Handle cancel button click
+        // }
+        
+        // private void OnSaveAndContinueButtonClicked(object sender, EventArgs e)
+        // {
+        //     // Handle save and continue button click
+        // }
+    }
+}
